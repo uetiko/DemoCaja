ac23272c1aed0c23a795f84a6127676df2eefd9b
Angel Barrientos <silent@uetiko.gateway.2wire.net>
2012-08-02 01:03:09 -0500

    js del panel

--- web/js/dashboard/dashboard.js
+++ web/js/dashboard/dashboard.js
@@ -3,7 +3,24 @@
  */
 
 function initEvents() {
+	uploadFile();
+}
 
+function uploadFile(){
+	$('#status').hide();
+	new AjaxUpload($('#updateFile'), {
+		action : '../../../src/com.calaveraStudio.demo.cgi/cgi.php',
+		name : 'file',
+		onSubmit : function(file, ext) {			
+			if((true != (/(jpg)$/.test(ext)))){
+				alert('No es una imagen en formato jpg, lo sorry');
+				return false;
+			}else{
+				$("div#status").show();
+				$("div#status").html('Procesando archivo. <img src="../../img/gif/loading.gif" />');
+			}
+		}
+	});
 }
 
 function publica() {
@@ -51,6 +68,7 @@ function getNoActivas() {
 	$.ajax({
 		url : '../../../src/com.calaveraStudio.demo.cgi/cgi.php',
 		type : 'POST',
+		dataType : 'json',
 		data : {
 			publicar : true,
 			action : 'noActivas',
